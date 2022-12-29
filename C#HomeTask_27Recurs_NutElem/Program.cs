eaf6e0c9ae0240d735863c7d8d72c153cfe147b2
Belikoff80 <maxim.belikoff@gmail.com>
2022-12-29 22:28:42 +0300

    added home tasks 64, 66, 68

---
+++ C#HomeTask_27Recurs_NutElem/Program.cs
@@ -0,0 +1,37 @@
+﻿// Задача 64: 
+// Задайте значение N. 
+// Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. 
+// Выполнить с помощью рекурсии.
+
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+//Ввод данных 
+int ReadData(string line)
+{
+    //output msg 
+    Console.Write(line);
+    int numberP = int.Parse(Console.ReadLine() ?? "0");
+    return numberP;
+}
+
+//Вывод результата
+void PrintResult(string line)
+{
+    Console.WriteLine(line);
+
+}
+
+//Вывод натуральных чисел от N до 1 с использованием рекурсии
+string LineGenRec(int num)
+{
+    if (num == 0) return string.Empty;
+    else
+    {
+        string outLine = num + " " + LineGenRec(num - 1);
+        return outLine;
+    }
+
+}
+
+int num = ReadData("input: ");
+PrintResult(LineGenRec(num));
---
+++ C#HomeTask_29_Recurs_Accerman/Program.cs
@@ -0,0 +1,39 @@
+﻿// Задача 68: 
+// Напишите программу вычисления функции Аккермана с помощью рекурсии. 
+// Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+// m = 3, n = 2 -> A(m,n) = 29
+
+
+//Ввод данных 
+int ReadData(string line)
+{
+    //output msg 
+    Console.Write(line);
+    int numberP = int.Parse(Console.ReadLine() ?? "0");
+    return numberP;
+}
+//Вывод результата
+void PrintResult(string line, int N)
+{
+    Console.WriteLine(line + N);
+
+
+}
+//Вывод суммы натуральных чисел в диапозоне от N до M с использованием рекурсии
+int RecAccerman(int N, int M)
+{
+    if (N == 0)
+        return M + 1;
+    else
+      if ((N != 0) && (M == 0))
+        return RecAccerman(N - 1, 1);
+    else
+        return RecAccerman(N - 1, RecAccerman(N, M - 1));
+}
+
+int N = ReadData("input N: ");
+int M = ReadData("input M: ");
+int result = RecAccerman(N, M);
+PrintResult("RESULT: ", result);
+
